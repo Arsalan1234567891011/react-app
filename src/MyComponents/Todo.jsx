b7de4d20d407975cc0e8abd559df02e1da77d177
M.Arsalan <malikarsalanhhg2244@gmail.com>
2025-11-26 11:41:36 +0500

    fix issue

--- src/MyComponents/Todo.jsx
+++ src/MyComponents/Todo.jsx
@@ -3,14 +3,14 @@ import TodoItem from "./TodoItem.jsx";
 import TodoItem1 from "./TodoItem1.jsx";
 
 export default function Todo() {
-
-  const [todos, setTodos] = useState([]);
-/*
+  
+const [todos, setTodos] = useState([]);
 useEffect(() => {
-  fetch("http://localhost:8003/api/bussiness?secret_key=1764034827202511250140274")
+  fetch("http://localhost:8003/api/bussiness?secret_key=176403482720251125014027")
     .then(response => response.json())
     .then(response => {
       setTodos(response.data);
+      //console.log(process.env.REACT_APP_API_BASE_URL); 
       if(response.status == false){
         Swal.fire({
         icon: "error",
@@ -23,10 +23,10 @@ useEffect(() => {
       console.error("Error fetching todos:", error);
     });
 }, []);
-*/
 
 
-  let todos1 = [
+
+ /* let todos1 = [
     {
       id : 1,
       title : "Title 1",
@@ -37,14 +37,14 @@ useEffect(() => {
       title : "Title 2",
       description : "Description 2",
     },
-  ];
+  ];*/
 
 
   return (
     <div className='row'>
       <div className='col-12'>
-       <TodoItem1 todosItems={todos1} />
-       {/*<TodoItem todosItems={todos} />*/}
+       {/*<TodoItem1 todosItems={todos1} />*/}
+       {<TodoItem todosItems={todos} />}
       </div>
     </div>
   );
