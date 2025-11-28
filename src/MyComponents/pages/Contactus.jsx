12ead779093de5e6579c475b158adaf1a8c9e9e3
M.Arsalan <malikarsalanhhg2244@gmail.com>
2025-11-28 12:02:33 +0500

    add react component

--- src/MyComponents/pages/Contactus.jsx
+++ src/MyComponents/pages/Contactus.jsx
@@ -1,13 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button, Flex, Splitter, Typography } from 'antd';
+
+// Small component
+export const Desc = (props) => (
+  <Flex justify="center" align="center" style={{ height: '100%' }}>
+    <Typography.Title type="secondary" level={5} style={{ whiteSpace: 'nowrap' }}>
+      {props.text}
+    </Typography.Title>
+  </Flex>
+);
+
+// Splitter Demo Component
+export const DemoSplitter = () => (
+  <Splitter style={{ height: 200, boxShadow: '0 0 10px rgba(3, 12, 2, 0.1)' }}>
+    <Splitter.Panel defaultSize="40%" min="20%" max="70%">
+      <Desc text="First" />
+    </Splitter.Panel>
+    <Splitter.Panel>
+      <Desc text="Second" />
+    </Splitter.Panel>
+  </Splitter>
+);
 
 export class Contactus extends Component {
   render() {
     return (
-      <h1 >This is contact us page </h1>
-    )
+      <>
+        <Flex gap="small" wrap>
+          <Button type="primary">Primary Button</Button>
+          <Button>Default Button</Button>
+          <Button type="dashed">Dashed Button</Button>
+          <Button type="text">Text Button</Button>
+          <Button type="link">Link Button</Button>
+        </Flex>
+        <br/>
+        <DemoSplitter />
+      </>
+    );
   }
 }
 
-export default Contactus
+export default Contactus;
+
 
 
